6bfc3f388e049d3028c188912b32bb3b598f6821
Lais Lopes <laisllopess21@gmail.com>
2026-03-25 11:52:13 -0300

    Criando models e prmeiro endpoint

---
+++ src/main/java/br/com/vollmed/api/model/endereco/Endereco.java
@@ -0,0 +1,7 @@
+package br.com.vollmed.api.model.endereco;
+
+public class Endereco {
+    
+    private String logradouro; 
+
+}
--- src/main/java/br/com/vollmed/api/model/medico/Medico.java
+++ src/main/java/br/com/vollmed/api/model/medico/Medico.java
@@ -1,15 +1,28 @@
 package br.com.vollmed.api.model.medico;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
+
+@Entity // SPRING JPA - Informa que a classe abaixo é uma entidade, ou seja, será uma tabela no BD. 
+@Table(name = "medicos") // SPRING JPA = (OPCIONAL) Altera o nome da tabela de medico para médicos
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Medico {
     
     // atributos -> caracteríticas -> variáveis 
-    String nome;
-    String email;
-    String telefone;
-    String crm;
-    Especialidade especialidade;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String crm;
+    private Especialidade especialidade;
 }
 
